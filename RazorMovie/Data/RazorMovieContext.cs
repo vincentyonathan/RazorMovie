8c3c81c54b705359e6ad63ba9c4daa91a58d47ee
vincentyonathan <vincentyonathan24@gmail.com>
2022-03-21 11:58:23 +0700

    Add project files.

---
+++ RazorMovie/Data/RazorMovieContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorMovie.Models;
+
+namespace RazorMovie.Data
+{
+    public class RazorMovieContext : DbContext
+    {
+        public RazorMovieContext (DbContextOptions<RazorMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorMovie.Models.Movie> Movie { get; set; }
+    }
+}
